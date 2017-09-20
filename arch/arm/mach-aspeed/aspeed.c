6faa31f9efbb8e36c6c7819f9f9c674c2d2f7e2d
Shawn TPE-BMC <None>
2017-09-20 10:45:01 +0800

    Merge Bug fix
    [:ks]
    Merge 5de68db -> Feature 359: UBB PDB EEPROM
    Merge fffecef -> Bug 326: Watchdog and Kernel reset should boot to u-boot

--- arch/arm/mach-aspeed/aspeed.c
+++ arch/arm/mach-aspeed/aspeed.c
@@ -145,8 +145,8 @@ static void __init do_sthelens_setup(void)
          * register, as the baud rates in aspeed.dtb all assume that the
          * divisor is active
          */
-        reg = readl(AST_IO(AST_BASE_SCU | 0x2c));
-        writel(reg | 0x00001000, AST_IO(AST_BASE_SCU | 0x2c));
+        //reg = readl(AST_IO(AST_BASE_SCU | 0x2c));
+        //writel(reg | 0x00001000, AST_IO(AST_BASE_SCU | 0x2c));
 
         //Disable host SPI mode  bit 12:13 = 00
         writel(0xFA1C84D6, AST_IO(AST_BASE_SCU | 0x70));
